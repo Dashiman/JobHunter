e19a8c1c5b93734b73f18acf08523b0da4248f62
Jakub Sarnowski <j.sarnowski@travactory.com>
2020-06-04 13:21:33 +0200

    service tests

--- JobHunterTests/ServiceTests/JobServiceTests.cs
+++ JobHunterTests/ServiceTests/JobServiceTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Model;
 using NUnit.Framework;
 using Services;
+using Services.VM;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -109,7 +110,135 @@ namespace JobHunterTests
         public async Task TakeJob_JobIdIsEmpty_ReturnsZero()
         {
             var service = serviceProvider.GetService<IJobService>();
-            var bidOffer = service.
+            var jobOffer = this.jobOffer;
+            jobOffer.Id = 0;
+            var result = await service.TakeJob(new BidOffer());
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task GetAll_WhenCalled_ReturnsJobList()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            var result = await service.GetAll();
+            Assert.IsNotNull(result);
+            Assert.True(result.Count >= 0);
+            Assert.IsInstanceOf<List<JobOffer>>(result);
+        }
+
+        [Test]
+        public async Task GetAll_JobListEmpty_ReturnsExceptionError()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.GetAll();
+            Assert.Throws<Exception>(() => throw new Exception());
+        }
+
+        [Test]
+        public async Task GetProfileData_WhenCalled_ReturnProfileDataObject()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.GetProfileData(1);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ProfileData>(result);
+        }
+
+        [Test]
+        public async Task GetProfileData_ProfileIsEmpty_ReturnsExceptionError()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.GetProfileData(0);
+            Assert.Throws<Exception>(() => throw new Exception());
+        }
+
+        [Test]
+        public async Task GetOffer_WhenCalled_ReturnsOfferObject()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            var result = await service.GetOffer(jobOffer.Id);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<JobOffer>(result);
+        }
+
+        [Test]
+        public async Task GetOffer_OfferIsEmpty_ReturnsExceptionError()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.GetOffer(1111);
+            Assert.IsNull(result);
+            Assert.Throws<Exception>(() => throw new Exception());
+        }
+
+        [Test]
+        public async Task DeleteOffer_WhenCalled_ReturnsOne()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            await service.Add(jobOffer);
+            var result = service.DeleteOffer(jobOffer.Id);
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task DeleteOffer_OfferNotExists_ReturnsExceptionError()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.DeleteOffer(1111);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task UpdateOffer_WhenCalled_ReturnsOne()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            await service.Add(jobOffer);
+            jobOffer.Title = "Updated job title";
+            jobOffer.Description = "Updated job description";
+            var result = await service.UpdateOffer(jobOffer);
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task UpdateOffer_OfferNotExists_ReturnsZero()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var result = await service.UpdateOffer(new JobOffer());
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task EndCourse_WhenCalled_ReturnsOne()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            await service.Add(jobOffer);
+            var endJobOfferModel = new EndModel { OfferId = jobOffer.Id, StatusId = jobOffer.Status };
+            var result = await service.EndCourse(endJobOfferModel);
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task EndCourse_OfferIsEmpty_ReturnsZero()
+        {
+            var service = serviceProvider.GetService<IJobService>();
+            var jobOffer = this.jobOffer;
+            await service.Add(jobOffer);
+            var endJobOfferModel = new EndModel { OfferId = 1111, StatusId = jobOffer.Status };
+            var result = await service.EndCourse(endJobOfferModel);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            user = null;
+            jobOffer = null;
+            bidOffer = null;
+            serviceCollection = null;
+            serviceProvider = null;
         }
     }
 }
